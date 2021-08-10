0d4887f4ae5cf3f7ee7a03dd380d7ef75a7ae851
ryo29wx <59174187+ryo29wx@users.noreply.github.com>
2021-08-10 19:29:51 +0900

    fix circleci22

--- src/db_test.go
+++ src/db_test.go
@@ -24,7 +24,7 @@ const (
 		"KEY `index03` (`category`)," +
 		"CONSTRAINT `seller_id` FOREIGN KEY (`seller_id`) REFERENCES `SELLER_INFO` (`seller_id`)" +
 		") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
-	insertQuery = "INSERT INTO PRODUCT_INFO(product_id, product_name, seller_id, stock, category, price, rate) VALUES ('test_productID', 'test_productName', 'test_sellerID', 5, 1, 100, 10)"
+	insertQuery = "INSERT INTO PRODUCT_INFO(product_id, product_name, seller_id, stock, category, price) VALUES ('test_productID', 'test_productName', 'test_sellerID', 5, 1, 100)"
 )
 
 func init() {
